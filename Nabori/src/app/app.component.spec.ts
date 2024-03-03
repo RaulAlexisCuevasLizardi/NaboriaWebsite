145c9179f7050822fabca50dd028a6f125a3f905
RaulAlexisCuevasLizardi <33424153+RaulAlexisCuevasLizardi@users.noreply.github.com>
2024-03-03 00:38:55 -0400

    Fixed title and modified side bar

--- Nabori/src/app/app.component.spec.ts
+++ Nabori/src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Nabori' title`, () => {
+  it(`should have the 'Naboria' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Nabori');
